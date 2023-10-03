1d02b365b9ab5765b5c8bfdd820b4de0aa359bda
Oksana Taysumova <otajsumova@gmail.com>
2023-10-03 20:38:42 +0300

    correction

--- src/components/RatesCards/RatesCards.jsx
+++ src/components/RatesCards/RatesCards.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import Item from '../Item/Item'
 import style from './slyle.module.scss'
-// import { useState } from "react";
+import { useState } from "react";
 
 
 
 export default function Cards(props) {
 
-    // const [id, setId] = useState(0)
+    const [id, setId] = useState(0)
 
     return (
         <div className={style.cards}>
             {props.arr.map((item, index) => (
-                <Item item={item} key={index} />))}
+                <Item item={item} key={index} activeCards={item.id === id} setId={setId} />))}
         </div>
     );
 }
