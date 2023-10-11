cad6fcbba6ae5e1af242249f53a1d2be65ca46d5
Oksana Taysumova <otajsumova@gmail.com>
2023-10-11 15:18:33 +0300

    correction

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,19 +6,18 @@ import style from "./style.module.scss"
 export default function Item(props) {
 
     // const [classIt, setClassIt] = useState(false);
-    const classItemActive = props.activeCards ? 'style.card1' : ""
-    const classItem = 'style.card'
+    const classItemActive = props.activeCards ? 'card1' : ""
+    const classItem = 'card'
     // const handleChange = () => {
     //     setClassIt(!classIt);
     // }
     function handleActive() {
         props.setId(props.item.id)
-        console.log(classItemActive)
     }
 
     return (
 
-        <section className={`${classItem} ${classItemActive}`} onClick={handleActive}>
+        <section className={`${style[classItem]} ${style[classItemActive]}`} onClick={handleActive}>
             <div className={style[props.item.color]}><h2 className={style.name}>{props.item.name}</h2></div>
             <div className={style[props.item.color]}><h2 className={style.price}>{props.item.price}</h2></div>
             <div ><h3 className={style.speed}>{props.item.speed}</h3></div>
